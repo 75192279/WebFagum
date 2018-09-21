f95c5a641f4a71418f98df597d7ba8e003f46e01
Rebeca <75192279@continental.edu.pe>
2018-09-21 16:12:43 -0500

    front end

--- webApp/resources/views/home/index.blade.php
+++ webApp/resources/views/home/index.blade.php
@@ -3,58 +3,58 @@
 <section class="slide1">
 		<div class="wrap-slick1">
 			<div class="slick1">
-				<div class="item-slick1 item1-slick1" style="background-image: url(/cliente/images/master-slide-02.jpg);">
+				<div class="item-slick1 item1-slick1" style="background-image: url(/cliente/img/Publicidad/slider-2.jpg);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="fadeInDown">
-							Women Collection 2018
+							Colecciones de estreno 2018
 						</span>
 
 						<h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-							New arrivals
+							Varones y mujeres
 						</h2>
 
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
 							<!-- Button -->
 							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-								Shop Now
+								Ver novedades
 							</a>
 						</div>
 					</div>
 				</div>
 
-				<div class="item-slick1 item2-slick1" style="background-image: url(/cliente/images/master-slide-03.jpg);">
+				<div class="item-slick1 item2-slick1" style="background-image: url(/cliente/img/Publicidad/wallpaper.wiki-American-Girl-Wallpapers-HD-PIC-WPC0013054.jpg);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rollIn">
-							Women Collection 2018
+						Colecciones de estreno 2018
 						</span>
 
 						<h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="lightSpeedIn">
-							New arrivals
+						Varones y mujeres
 						</h2>
 
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
 							<!-- Button -->
 							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-								Shop Now
+							Ver novedades
 							</a>
 						</div>
 					</div>
 				</div>
 
-				<div class="item-slick1 item3-slick1" style="background-image: url(/cliente/images/master-slide-04.jpg);">
+				<div class="item-slick1 item3-slick1" style="background-image: url(/cliente/img/Publicidad/clarissa4.jpg);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rotateInDownLeft">
-							Women Collection 2018
+						Colecciones de estreno 2018
 						</span>
 
 						<h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="rotateInUpRight">
-							New arrivals
+						Varones y mujeres
 						</h2>
 
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="rotateIn">
 							<!-- Button -->
 							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-								Shop Now
+							Ver novedades
 							</a>
 						</div>
 					</div>
@@ -71,50 +71,24 @@
 				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
 					<!-- block1 -->
 					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="/cliente/images/banner-02.jpg" alt="IMG-BENNER">
+						<img src="/cliente/img/Mujeres/00111935202926____1__516x640.jpg" alt="IMG-BENNER">
 
 						<div class="block1-wrapbtn w-size2">
 							<!-- Button -->
 							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Dresses
+								Top
 							</a>
 						</div>
 					</div>
 
 					<!-- block1 -->
 					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="/cliente/images/banner-05.jpg" alt="IMG-BENNER">
+						<img src="/cliente/img/Mujeres/top-deportivo-reebok-mujer-talla-s.jpg" alt="IMG-BENNER">
 
 						<div class="block1-wrapbtn w-size2">
 							<!-- Button -->
 							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Sunglasses
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="/cliente/images/banner-03.jpg" alt="IMG-BENNER">
-
-						<div class="block1-wrapbtn w-size2">
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Watches
-							</a>
-						</div>
-					</div>
-
-					<!-- block1 -->
-					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="/cliente/images/banner-07.jpg" alt="IMG-BENNER">
-
-						<div class="block1-wrapbtn w-size2">
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Footerwear
+								Top Reebok 
 							</a>
 						</div>
 					</div>
@@ -123,33 +97,59 @@
 				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
 					<!-- block1 -->
 					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="/cliente/images/banner-04.jpg" alt="IMG-BENNER">
+						<img src="/cliente/img/Mujeres/top-trasparencia-goma-personalizada.jpg" alt="IMG-BENNER">
 
 						<div class="block1-wrapbtn w-size2">
 							<!-- Button -->
 							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Bags
+								Top Transparente
+							</a>
+						</div>
+					</div>
+
+					<!-- block1 -->
+					<div class="block1 hov-img-zoom pos-relative m-b-30">
+						<img src="/cliente/img/Mujeres/gallery_001281_1.JPG" alt="IMG-BENNER">
+
+						<div class="block1-wrapbtn w-size2">
+							<!-- Button -->
+							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+								Top Reebok
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
+					<!-- block1 -->
+					<div class="block1 hov-img-zoom pos-relative m-b-30">
+						<img src="/cliente/img/Mujeres/top-deportivo-de-punto-mujer-con-diseno-cruzado-espalda-reforzada-gris-vigore-verde.jpg" alt="IMG-BENNER">
+
+						<div class="block1-wrapbtn w-size2">
+							<!-- Button -->
+							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+								Top deportivo
 							</a>
 						</div>
 					</div>
 
 					<!-- block2 -->
 					<div class="block2 wrap-pic-w pos-relative m-b-30">
-						<img src="/cliente/images/icons/bg-01.jpg" alt="IMG">
+						<img src="/cliente/img/Mujeres/30_ropa_deportiva-500x354.jpg" alt="IMG">
 
 						<div class="block2-content sizefull ab-t-l flex-col-c-m">
 							<h4 class="m-text4 t-center w-size3 p-b-8">
-								Sign up & get 20% off
+								30% de descuento
 							</h4>
 
 							<p class="t-center w-size4">
-								Be the frist to know about the latest fashion news and get exclu-sive offers
+								Ropas deportivas
 							</p>
 
 							<div class="w-size2 p-t-25">
 								<!-- Button -->
 								<a href="#" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
-									Sign Up
+									Ver novedades
 								</a>
 							</div>
 						</div>
@@ -164,7 +164,7 @@
 		<div class="container">
 			<div class="sec-title p-b-60">
 				<h3 class="m-text5 t-center">
-					Featured Products
+					Nuevas colecciones
 				</h3>
 			</div>
 
@@ -176,7 +176,7 @@
 						<!-- Block2 -->
 						<div class="block2">
 							<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
-								<img src="/cliente/images/item-02.jpg" alt="IMG-PRODUCT">
+								<img src="/cliente/img/Varons/2018-nuevos-hombres-camisa-de-deporte-gimnasio-baloncesto-running-ropa-deportiva-fútbol-Bodybuilding-compresión-masculina-camisa.jpg" alt="IMG-PRODUCT">
 
 								<div class="block2-overlay trans-0-4">
 									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
@@ -187,7 +187,7 @@
 									<div class="block2-btn-addcart w-size1 trans-0-4">
 										<!-- Button -->
 										<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-											Add to Cart
+											Adquirir
 										</button>
 									</div>
 								</div>
@@ -195,11 +195,11 @@
 
 							<div class="block2-txt p-t-20">
 								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Herschel supply co 25l
+									Diseño extranjera
 								</a>
 
 								<span class="block2-price m-text6 p-r-5">
-									$75.00
+									$20.00
 								</span>
 							</div>
 						</div>
@@ -209,7 +209,7 @@
 						<!-- Block2 -->
 						<div class="block2">
 							<div class="block2-img wrap-pic-w of-hidden pos-relative">
-								<img src="/cliente/images/item-03.jpg" alt="IMG-PRODUCT">
+								<img src="/cliente/img/Legins/calza-explo-depor-fitness1-8561ed6e9ca772e98215312781670107-320-0.jpg" alt="IMG-PRODUCT">
 
 								<div class="block2-overlay trans-0-4">
 									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
@@ -220,7 +220,7 @@
 									<div class="block2-btn-addcart w-size1 trans-0-4">
 										<!-- Button -->
 										<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-											Add to Cart
+											Adquirir
 										</button>
 									</div>
 								</div>
@@ -228,11 +228,11 @@
 
 							<div class="block2-txt p-t-20">
 								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Denim jacket blue
+									Leggins coleccion
 								</a>
 
 								<span class="block2-price m-text6 p-r-5">
-									$92.50
+									$15.60
 								</span>
 							</div>
 						</div>
@@ -242,7 +242,7 @@
 						<!-- Block2 -->
 						<div class="block2">
 							<div class="block2-img wrap-pic-w of-hidden pos-relative">
-								<img src="/cliente/images/item-05.jpg" alt="IMG-PRODUCT">
+								<img src="/cliente/img//Pelotas/bola-de-futebol-society-nike-cbf-sp13-img.jpg" alt="IMG-PRODUCT">
 
 								<div class="block2-overlay trans-0-4">
 									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
@@ -253,7 +253,7 @@
 									<div class="block2-btn-addcart w-size1 trans-0-4">
 										<!-- Button -->
 										<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-											Add to Cart
+											Adquirir
 										</button>
 									</div>
 								</div>
@@ -261,11 +261,11 @@
 
 							<div class="block2-txt p-t-20">
 								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Coach slim easton black
+									Balòn de coleccion - Nike
 								</a>
 
 								<span class="block2-price m-text6 p-r-5">
-									$165.90
+									$25.50
 								</span>
 							</div>
 						</div>
@@ -275,7 +275,7 @@
 						<!-- Block2 -->
 						<div class="block2">
 							<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelsale">
-								<img src="/cliente/images/item-07.jpg" alt="IMG-PRODUCT">
+								<img src="/cliente/img/Varons/t_shirt_860_homme_jaune_badminton_table_tennis_padel_squash_artengo_8360700_984113.jpg" alt="IMG-PRODUCT">
 
 								<div class="block2-overlay trans-0-4">
 									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
@@ -286,7 +286,7 @@
 									<div class="block2-btn-addcart w-size1 trans-0-4">
 										<!-- Button -->
 										<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-											Add to Cart
+											Adquirir
 										</button>
 									</div>
 								</div>
@@ -294,15 +294,15 @@
 
 							<div class="block2-txt p-t-20">
 								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Frayed denim shorts
+									Rebaja
 								</a>
 
 								<span class="block2-oldprice m-text7 p-r-5">
-									$29.50
+									$35.50
 								</span>
 
 								<span class="block2-newprice m-text8 p-r-5">
-									$15.90
+									$25.90
 								</span>
 							</div>
 						</div>
